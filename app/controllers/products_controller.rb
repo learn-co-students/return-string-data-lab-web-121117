1d2b119669c56647b44f036f7320f8bed2177cc3
julientregoat <julienctregoat@gmail.com>
2018-02-06 23:02:24 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,41 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    if @product.inventory != nil
+      if @product.inventory > 0
+        result = true
+      else
+        result = false
+      end
+    else
+      result = false
+    end
+    render plain:"#{result}"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :inventory)
+  end
+
 end
